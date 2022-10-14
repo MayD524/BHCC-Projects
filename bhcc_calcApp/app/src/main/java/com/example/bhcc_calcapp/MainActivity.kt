dec43d4a36fdd459f6adf9cbb30d1ea13081bf84
MayD524 <crossroadsact@gmail.com>
2022-10-14 13:08:57 -0400

    init project 2 (not running android studio issue

---
+++ bhcc_calcApp/app/src/main/java/com/example/bhcc_calcapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.bhcc_calcapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.bhcc_calcapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
