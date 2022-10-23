79dce4cc744e235a53c9051126c0c993dc9eb2f4
May <57610348+RyanD524@users.noreply.github.com>
2022-10-22 20:25:23 -0400

    feature complete

--- bhcc_calcApp2/main.py
+++ bhcc_calcApp2/main.py
@@ -1,6 +1,9 @@
-from math import *
+from math import log10 as log, log1p as ln, sqrt, exp
 import kivy 
+import re
     
+
+
 kivy.require('2.1.0')
 
 from kivy.app import App
@@ -16,19 +19,58 @@ class HistoryPopup(Popup):
     def on_open(self):
         self.ids.data_label.text = "\n".join([f'[{i}] {history[i]}' for i in range(len(history))]) if history else "No History"
         
-class CalcGridLayout(GridLayout):
+class CalcLayout(GridLayout):
 
     def calculate(self, calculation):
         if calculation:
             try:
                 self.display.text = str(eval(calculation))
-                history.append(f'{calculation}={self.display.text}')
+                data = f'{calculation}={self.display.text}'
+                if data not in history:
+                    history.append(data)
             except Exception:
                 self.display.text = "Error"
+    
+    def lastNumber(self) -> str:
+        dt = self.display.text
+        x = re.findall(r'\d+', dt)
+        if x:
+            return x[-1]
+        return None
+    
+    def addSqrt(self) -> None:
+        dt = self.lastNumber()
+        if dt == None:
+            return
+        self.display.text = self.display.text.replace(dt, f"sqrt({dt})")
+        
+    def power(self) -> None:
+        self.dispaly.text += "**"
+    
+    def square(self) -> None:
+        self.display.text += "**2"
+    
+    def log(self) -> None:
+        dt = self.lastNumber()
+        if dt == None:
+            return
+        self.display.text = self.display.text.replace(dt, f"log({dt})")
+    
+    def nthRoot(self) -> None:
+        dt = self.lastNumber()
+        if dt == None:
+            return
+        self.display.text = self.display.text.replace(dt, f"(1/{dt})") 
+    
+    def ln(self) -> None:
+        dt = self.lastNumber()
+        if dt == None:
+            return
+        self.display.text = self.display.text.replace(dt, f"ln({dt})")
 
 class CalculatorApp(App):
     def build(self):
-        return CalcGridLayout()
+        return CalcLayout()
 
 # creating object and running it
 calcApp = CalculatorApp()
